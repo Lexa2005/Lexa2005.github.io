1b10cd9f1f0ed52e68f03196626f70fc192b8a81
RapanderesIT <105642668+RapanderesIT@users.noreply.github.com>
2024-10-11 11:29:23 +0200

    aaoao2

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const card = document.querySelector('.card');
-    setTimeout(() => {
-        card.classList.add('visible');
-    }, 500);
-});

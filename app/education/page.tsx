2f8b8dd1e800a21dc20e56a22286cbc9a60800b4
Your Name <you@example.com>
2024-10-25 14:35:03 +0200

    gghh

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -1,5 +1,10 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 export default function Projects () {
     return <div>
+        <Header />
         <h1>hell</h1>
+        <Footer />
     </div>
 }
